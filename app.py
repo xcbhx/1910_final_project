ad22f9063d1d728de7d1557a99a1e9cbaccdeb23
xchbx <ceina.ellison@gmail.com>
2024-12-09 11:28:59 -0500

    edited base.html

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('home.html')
+def home():
+    logged_in = request.args.get('logged_in') == 'true'
+    return render_template('home.html', logged_in=logged_in)
 
 @app.route('/explore')
 def explore_site():
     return render_template('explore.html')
 
 
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def sign_in():
+    if request.method == 'POST':
+        return redirect(url_for('home', logged_in='true'))
     return render_template('signin.html')
 
 
-@app.route('/join')
+@app.route('/join', methods=['GET', 'POST'])
 def join_membership():
+    if request.method == 'POST':
+        return redirect(url_for('home', logged_in='true'))
     return render_template('join.html')
 
 @app.route('/profile', methods=['GET','POST'])
 def user_profile():
+    logged_in = request.args.get('logged_in') == 'true'
     user_response = None
     if request.method == 'POST':
         user_response = request.form.get('user') # Fetch the value of the selected radio button
 
     context = {
         'username': 'Ceina Ellison',
-        'user': user_response
+        'user': user_response,
+        'logged_in': logged_in
     }
 
     return render_template('profile.html', **context)
